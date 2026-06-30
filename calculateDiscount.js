13518a4c901c33a8bf7771e401e1cbeb2b5ed6f0
Sammy Tu <Sammy_Tu@wiwynn.com>
2026-06-30 15:17:50 -0600

    initial commit with tests and CI workflow

---
+++ calculateDiscount.js
@@ -0,0 +1,17 @@
+function calculateDiscount(price, memberType) {
+  if (price <= 0) {
+    throw new Error('Price must be positive');
+  }
+
+  const discounts = {
+    gold: 0.8,
+    silver: 0.9,
+  };
+
+  const multiplier = discounts[memberType] ?? 1;
+  const finalPrice = price * multiplier;
+
+  return Math.round(finalPrice * 100) / 100;
+}
+
+module.exports = { calculateDiscount };
